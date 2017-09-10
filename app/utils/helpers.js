ae69ccc06448b42d6b5b51503ff7a068578d2df6
vilkoz <vilko.a@gmail.com>
2017-09-10 13:44:12 +0300

    change ip in helpers

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -4,7 +4,7 @@ export const getUserInfo = (username) => axios.get(`https://api.github.com/users
 
 export const getImages = (images) => axios.get(`https://api.github.com/users/${images}`); // TODO add instagraam API
 export const getImagesTemp = (user, id) => (
-	axios.get(`http://10.1.49.126:9999/proxy.php?user=${user}&id=${id}`).then((res) => (
+	axios.get(`http://192.168.0.246:9999/proxy.php?user=${user}&id=${id}`).then((res) => (
 			{
 				images: res.data.items.map((t) => (t.images.standard_resolution.url)),
 				usernames: res.data.items.map((t) => (t.username)),
